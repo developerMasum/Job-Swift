ca7f928a8547f1d9c131d729e0197223d361de87
anik12136 <anikbiswas431@gmail.com>
2023-08-22 21:44:18 +0600

    forgot page responsive

--- client/src/Pages/ForgotPassword/ForgotPassword.jsx
+++ client/src/Pages/ForgotPassword/ForgotPassword.jsx
@@ -19,17 +19,17 @@ const ForgotPassword = () => {
 
     return (
       <div className=''>
-         <div className=' bg-fuchsia-950 my-2 rounded-tl-[100px] rounded-br-[100px]'>
-         <div className='py-40 px-40'>
-            <div className=' py-20 forgotPasswordBG h-[100%] w-[100%] rounded-tl-[100px] rounded-br-[100px]'>
+         <div className=' bg-fuchsia-950 my-2 lg:md:rounded-tl-[100px] lg:md:rounded-br-[100px]'>
+         <div className='lg:md:py-40 lg:md:px-40 p-10 px-5'>
+            <div className=' py-20 forgotPasswordBG h-[100%] w-[100%] lg:md:rounded-tl-[100px] lg:md:rounded-br-[100px]'>
                <div>
-               <form className='lg:md:ps-20' onSubmit={handleSubmit((data) => console.log(data))}>
-               <h2 className='text-white text-3xl font-bold mb-5'>Forgot Your Password ?</h2>
+               <form className='lg:md:ps-20 ps-3' onSubmit={handleSubmit((data) => console.log(data))}>
+               <h2 className='text-white lg:md:text-3xl font-bold mb-5'>Forgot Your Password ?</h2>
 
                     <div className='pt-5'>
                     <h2 className='text-white '>Enter your email</h2>
 
-                        <input className='mt-2 border border-[#c4cfde] rounded-md w-[300px] h-10' {...register('email', { required: true })} />
+                        <input className='mt-2 border border-[#c4cfde] rounded-md lg:md:w-[300px] h-10' {...register('email', { required: true })} />
                         {errors.company && <p className='text-white mt-1'>Email is required.</p>}
                     </div>
                     {/* <div className='mt-5'>
@@ -38,7 +38,7 @@ const ForgotPassword = () => {
                         {errors.company && <p className='text-red-400 mt-1'>Email is required.</p>}
                     </div> */}
                     <div className='py-6 flex items-center '>
-                        <input className='btn btn-outline bg-amber-500 w-[300px]' type="submit" />
+                        <input className='btn btn-outline bg-amber-500 lg:md:w-[300px]' type="submit" />
                     </div>
                     <Link to='/login' className='text-white flex items-center hover:text-amber-400'> <BiLeftArrowAlt></BiLeftArrowAlt>Back to sign in</Link>
                 </form>
