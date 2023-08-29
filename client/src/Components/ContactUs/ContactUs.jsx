bf6fc0c1b9a44d28f1f9779d2b6b72fdd9c62014
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-08-30 01:44:05 +0600

    added form

--- client/src/Components/ContactUs/ContactUs.jsx
+++ client/src/Components/ContactUs/ContactUs.jsx
@@ -1,10 +1,15 @@
 import ContactCard from "./ContactCard";
+import ContactForm from "./ContactForm";
 
 const ContactUs = () => {
   return (
     <>
       <h1 className="text-5xl font-bold text-center my-8">Contact Us</h1>
       <ContactCard />
+      <div className="mt-12 md:flex justify-between gap-4">
+        <ContactForm/>
+    
+      </div>
     </>
   );
 };
