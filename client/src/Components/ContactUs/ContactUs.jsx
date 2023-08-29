c774a311a27514ef05cf2dfff3909b53933d7afb
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-08-30 00:31:24 +0600

    added contact us component

---
+++ client/src/Components/ContactUs/ContactUs.jsx
@@ -0,0 +1,10 @@
+
+const ContactUs = () => {
+    return (
+        <div>
+            <p>This is contact Us file</p>
+        </div>
+    );
+};
+
+export default ContactUs;
--- client/src/Components/ProgramManager/ProgramManager.jsx
+++ client/src/Components/ProgramManager/ProgramManager.jsx
@@ -8,20 +8,28 @@ import img4 from "../../assets/Image/programManager4.png";
 const ProgramManager = () => {
   return (
     <div>
+      {/* Heading */}
       <h1 className="text-5xl font-bold text-center line leading-tight">
         Manage your entire process, <br />
         from sourcing to employee onboarding <br />
         and management
       </h1>
+      {/* Grid layout for cards */}
       <div className="grid md:grid-cols-2 gap-8 md:gap-16 my-8">
         {/* card 01 */}
         <div className="transition duration-1000 ease-in-out transform hover:scale-105 ps-4">
-          <img className="mx-auto px-4 md:max-w-full md:h-[500px]" src={img1} alt="" />
+          <img
+            className="mx-auto px-4 md:max-w-full md:h-[500px]"
+            src={img1}
+            alt=""
+          />
           <div className="text-center mt-12">
             <h4 className="text-4xl text-red-400 font-bold mb-2">
               SOURCE & ATTRACT
             </h4>
-            <h4 className="text-4xl font-semibold">Find and attract candidates</h4>
+            <h4 className="text-4xl font-semibold">
+              Find and attract candidates
+            </h4>
             <p className="text-gray-500 font-semibold my-6">
               Fill your pipeline quickly with one-click job posting to 200+
               sites, AI-powered sourcing, employee referrals and more.
@@ -34,12 +42,18 @@ const ProgramManager = () => {
         </div>
         {/* card 02 */}
         <div className="transition duration-1000 ease-in-out transform hover:scale-105 ps-4">
-          <img className="mx-auto px-4 md:max-w-full md:h-[500px]" src={img2} alt="" />
+          <img
+            className="mx-auto px-4 md:max-w-full md:h-[500px]"
+            src={img2}
+            alt=""
+          />
           <div className="text-center mt-12">
             <h4 className="text-4xl text-blue-400 font-bold mb-2">
               EVALUATE & COLLABORATE
             </h4>
-            <h4 className="text-4xl font-semibold">Move the right applicants forward</h4>
+            <h4 className="text-4xl font-semibold">
+              Move the right applicants forward
+            </h4>
             <p className="text-gray-500 font-semibold my-6">
               Collaborate with hiring teams to evaluate applicants, gather
               feedback and decide who's best, all in one recruiting system.
@@ -52,12 +66,18 @@ const ProgramManager = () => {
         </div>
         {/* card 03 */}
         <div className="transition duration-1000 ease-in-out transform hover:scale-105 ps-4">
-          <img className="mx-auto px-4 md:max-w-full md:h-[500px]" src={img3} alt="" />
+          <img
+            className="mx-auto px-4 md:max-w-full md:h-[500px]"
+            src={img3}
+            alt=""
+          />
           <div className="text-center mt-12">
             <h4 className="text-4xl text-indigo-500 font-bold mb-2">
               AUTOMATE & HIRE
             </h4>
-            <h4 className="text-4xl font-semibold">Make the best hire, in half the time</h4>
+            <h4 className="text-4xl font-semibold">
+              Make the best hire, in half the time
+            </h4>
             <p className="text-gray-500 font-semibold my-6">
               Fill your pipeline quickly with one-click job posting to 200+
               sites, AI-powered sourcing, employee referrals and more.
@@ -70,7 +90,11 @@ const ProgramManager = () => {
         </div>
         {/* card 04 */}
         <div className="transition duration-1000 ease-in-out transform hover:scale-105 ps-4">
-          <img className="mx-auto px-4 md:max-w-full md:h-[500px]" src={img4} alt="" />
+          <img
+            className="mx-auto px-4 md:max-w-full md:h-[500px]"
+            src={img4}
+            alt=""
+          />
           <div className="text-center mt-12">
             <h4 className="text-4xl text-teal-400 font-bold mb-2">
               ONBOARD & MANAGE
