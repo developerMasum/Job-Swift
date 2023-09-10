d5490442f7e1526e51550a2f48e90a639e86e078
developerMasum <developer.masum2001@gmail.com>
2023-09-10 15:57:18 +0600

    test

--- client/src/redux/candidates/candidatesOperation.js
+++ client/src/redux/candidates/candidatesOperation.js
@@ -1,21 +1,3 @@
-// import { createAsyncThunk } from "@reduxjs/toolkit";
-// import axios from "axios";
-
-// export const getAllCandidates = createAsyncThunk(
-//   "getAllCandidates",
-//   async (_, { rejectWithValue }) => {
-//     try {
-//       const response = await axios.get(
-//         `http://localhost:5000/all-applications?sortOrder=${sortOrder}`
-//       );
-//       console.log(response.data);
-//       return response.data; // Return the data received from the server if needed
-//     } catch (error) {
-//       // Return the error payload using rejectWithValue
-//       return rejectWithValue(error.response.data);
-//     }
-//   }
-// );
 
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
