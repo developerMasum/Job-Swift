1be8fb212e6a7d22f7eeb9fc14761bf067909524
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-09-12 16:28:34 +0600

    added new news portal

--- client/src/Components/Overview/SocialLink.jsx
+++ client/src/Components/Overview/SocialLink.jsx
@@ -6,12 +6,11 @@ import {
   RiLinkedinBoxFill,
   RiWhatsappFill,
   RiTwitterFill,
-  RiPinterestFill,
-  RiSnapchatFill, 
-  RiTiktokFill, 
-  RiRedditFill, 
-  RiYoutubeFill, 
+  RiLinksFill,
 } from "react-icons/ri";
+import bdjobs from "../../assets/Image/bdjobs.png"
+import glassdoor from "../../assets/Image/glassdoor.png"
+import monster from "../../assets/Image/monster.png"
 
 const SocialLink = () => {
   return (
@@ -24,14 +23,12 @@ const SocialLink = () => {
         className="text-blue-500 mx-2 my-2 transition-transform duration-300 ease-in-out transform hover:scale-110 hover:rotate-3" // Tailwind CSS classes for styling and hover effect
       >
         <div className="flex flex-col items-center">
-          
           {/* Flex container */}
           <RiFacebookFill size={50} className="text-2xl" />
           {/* Facebook icon */}
-          <p>Share on Facebook</p> 
+          <p>Share on Facebook</p>
         </div>
       </a>
-
       {/* Instagram */}
       <a
         href="https://www.instagram.com/your-instagram-profile-url"
@@ -42,10 +39,9 @@ const SocialLink = () => {
         <div className="flex flex-col items-center">
           <RiInstagramFill size={50} className="text-2xl" />
           {/* Instagram icon */}
-          <p>Share on Instagram</p> 
+          <p>Share on Instagram</p>
         </div>
       </a>
-
       {/* Github */}
       <a
         href="https://www.github.com/your-github-profile-url"
@@ -55,10 +51,9 @@ const SocialLink = () => {
       >
         <div className="flex flex-col items-center">
           <RiGithubFill size={50} className="text-2xl" /> {/* GitHub icon */}
-          <p>Share on Github</p> 
+          <p>Share on Github</p>
         </div>
       </a>
-
       {/* LinkedIn */}
       <a
         href="https://www.linkedin.com/in/your-linkedin-profile-url"
@@ -69,10 +64,9 @@ const SocialLink = () => {
         <div className="flex flex-col items-center">
           <RiLinkedinBoxFill size={50} className="text-2xl" />
           {/* LinkedIn icon */}
-          <p>Share on Linkedin</p> 
+          <p>Share on Linkedin</p>
         </div>
       </a>
-
       {/* WhatsApp */}
       <a
         href="https://wa.me/your-whatsapp-phone-number"
@@ -83,10 +77,9 @@ const SocialLink = () => {
         <div className="flex flex-col items-center">
           <RiWhatsappFill size={50} className="text-2xl" />
           {/* WhatsApp icon */}
-          <p>Share on Whatsapp</p> 
+          <p>Share on Whatsapp</p>
         </div>
       </a>
-
       {/* Twitter */}
       <a
         href="https://twitter.com/your-twitter-profile-url"
@@ -96,74 +89,72 @@ const SocialLink = () => {
       >
         <div className="flex flex-col items-center">
           <RiTwitterFill size={50} className="text-2xl" /> {/* Twitter icon */}
-          <p>Share on Twitter</p> 
+          <p>Share on Twitter</p>
         </div>
       </a>
-
-      {/* Pinterest */}
+      {/* BD Jobs */}
       <a
-        href="https://www.pinterest.com/your-pinterest-profile-url"
+        href="https://www.bdjobs.com"
         target="_blank"
         rel="noopener noreferrer"
-        className="text-red-600 mx-2 my-2 transition-transform duration-300 ease-in-out transform hover:scale-110 hover:-rotate-3"
+        className="text-purple-500 mx-2 my-2 transition-transform duration-300 ease-in-out transform hover:scale-110 hover:-rotate-3"
       >
         <div className="flex flex-col items-center">
-          <RiPinterestFill size={50} className="text-2xl" />
-          {/* Pinterest icon */}
-          <p>Share on Pinterest</p> 
+          {/* You can replace the icon with an appropriate BD Jobs icon */}
+          <img
+            src={bdjobs}
+            alt="BD Jobs"
+            className=" rounded-full text-2xl"
+            style={{ width: "50px", height: "50px" }}
+          />
+          <p>Share on BD Jobs</p>
         </div>
       </a>
-
-      {/* Snapchat */}
+      {/* Indeed */}
       <a
-        href="https://www.snapchat.com/your-snapchat-profile-url"
+        href="https://www.indeed.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-green-500 mx-2 my-2 transition-transform duration-300 ease-in-out transform hover:scale-110 hover:-rotate-3"
+      >
+        <div className="flex flex-col items-center">
+          <RiLinksFill size={50} className="text-2xl" />{" "}
+          <p>Share Jobs on Indeed</p>
+        </div>
+      </a>
+      {/* Monster */}
+      <a
+        href="https://www.monster.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-purple-500 mx-2 my-2 transition-transform duration-300 ease-in-out transform hover:scale-110 hover:-rotate-3"
+      >
+        <div className="flex flex-col items-center">
+          <img
+            src={monster}
+            alt="Monster"
+            className=" rounded-full text-2xl"
+            style={{ width: "50px", height: "50px" }}
+          />
+          <p>Find Jobs on Monster</p>
+        </div>
+      </a>
+      \ {/* Glassdoor */}
+      <a
+        href="https://www.glassdoor.com"
         target="_blank"
         rel="noopener noreferrer"
         className="text-yellow-500 mx-2 my-2 transition-transform duration-300 ease-in-out transform hover:scale-110 hover:-rotate-3"
       >
         <div className="flex flex-col items-center">
-          <RiSnapchatFill size={50} className="text-2xl" />
-          {/* Snapchat icon */}
-          <p>Share on Snapchat</p> 
-        </div>
-      </a>
-
-      {/* TikTok */}
-      <a
-        href="https://www.tiktok.com/your-tiktok-profile-url"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-pink-500 mx-2 my-2 transition-transform duration-300 ease-in-out transform hover:scale-110 hover:-rotate-3"
-      >
-        <div className="flex flex-col items-center">
-          <RiTiktokFill size={50} className="text-2xl" /> {/* TikTok icon */}
-          <p>Share on TikTok</p> 
-        </div>
-      </a>
-
-      {/* Reddit */}
-      <a
-        href="https://www.reddit.com/your-reddit-profile-url"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-orange-500 mx-2 my-2 transition-transform duration-300 ease-in-out transform hover:scale-110 hover:-rotate-3"
-      >
-        <div className="flex flex-col items-center">
-          <RiRedditFill size={50} className="text-2xl" /> {/* Reddit icon */}
-          <p>Share on Reddit</p> 
-        </div>
-      </a>
-
-      {/* YouTube */}
-      <a
-        href="https://www.youtube.com/your-youtube-profile-url"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-red-500 mx-2 my-2 transition-transform duration-300 ease-in-out transform hover:scale-110 hover:-rotate-3"
-      >
-        <div className="flex flex-col items-center">
-          <RiYoutubeFill size={50} className="text-2xl" /> {/* YouTube icon */}
-          <p>Share on YouTube</p> 
+          {/* You can replace the icon with an appropriate Glassdoor icon */}
+          <img
+            src={glassdoor}
+            alt="Glassdoor"
+            className=" rounded-full text-2xl"
+            style={{ width: "50px", height: "50px" }}
+          />
+          <p>Find Jobs on Glassdoor</p>
         </div>
       </a>
     </div>
