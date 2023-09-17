99c766ecd131cf2eab530a3d1bd5bb0a48aa48ba
Md Arifur Rahman <sojibkhanarif999@gmail.com>
2023-09-17 13:50:53 +0600

    search field add

--- client/src/redux/application/api.js
+++ client/src/redux/application/api.js
@@ -24,7 +24,7 @@ export const getAllApplications = createAsyncThunk(
   async (_, { rejectWithValue }) => {
     try {
       const response = await axios.get(
-        " https://server-job-swift.vercel.app/all-applications"
+        "http://localhost:5000/all-applications"
       );
       console.log(response.data);
       return response.data; // Return the data received from the server if needed
