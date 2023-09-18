b647f75006e81d81f99982ef00ac43de7ad25fc1
developerMasum <developer.masum2001@gmail.com>
2023-09-19 00:40:37 +0600

    fixed vercel deploy error

--- Server/index.js
+++ Server/index.js
@@ -1,20 +1,11 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const multer = require("multer");
 require("dotenv").config();
-const path = require("path");
 const sendMail = require("./sendMail");
 const jwt = require("jsonwebtoken");
-const fs = require("fs");
-const fileUpload = require("express-fileupload");
-
 const port = process.env.PORT || 5000;
-const uploadPath = path.join(__dirname, "public", "images"); // Specify the destination directory for images
 
-if (!fs.existsSync(uploadPath)) {
-  fs.mkdirSync(uploadPath, { recursive: true });
-}
 
 // middleware
 const corsOptions = {
@@ -25,20 +16,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use(fileUpload());
 
-
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'https://job-swift-git-masum-developermasum.vercel.app'); 
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   next();
-// });
-
-const uploadDirectory = path.join(__dirname, "public", "uploads");
-app.use(express.static(uploadDirectory));
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const { log } = require("console");
@@ -66,28 +44,11 @@ const verifyJWT = (req, res, next) => {
   });
 };
 
-// ATSWebsite atswebsite2023
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mq0mae1.mongodb.net/?retryWrites=true&w=majority`
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/images");
-  },
-  filename: (req, file, cb) => {
-    cb(
-      null,
-      file.fieldname + "_" + Date.now() + path.extname(file.originalname)
-    );
-  },
-});
 
-const upload = multer({ storage });
 
 // node mailer --mailing section --
 app.post("/mail", sendMail);
-// app.get('/show-mail', async(req,res)=>{
-//   const result = await UserCollection.find().toArray();
-//   res.send(result);
-// })
+
 
 const uri =
   "mongodb+srv://ATSWebsite:atswebsite2023@cluster0.3besjfn.mongodb.net/?retryWrites=true&w=majority";
@@ -121,58 +82,10 @@ async function run() {
       res.send({ token });
     });
 
-    // use express file-upload
-
-
-    // app.post("/upload-new", async (req, res) => {
-    //   try {
-    //     if (!req.files) {
-    //       return res.status(400).json({ message: "No files were uploaded." });
-    //     }
-    
-    //     const imageFile = req.files.image;
-    //     const resumeFile = req.files.resume;
-    //     const formData = req.body;
-    
-    //     // Read image and resume files as base64
-    //     const imageBase64 = imageFile.data.toString("base64");
-    //     const resumeBase64 = resumeFile.data.toString("base64");
-    
-    //     // Create an array of documents to insert
-    //     const documentsToInsert = {
-    //       jobTitle: formData.jobTitle,
-    //       stage: formData.stage,
-    //       jobPosterEmail: formData.jobPosterEmail,
-    //       jobId: formData.jobId,
-    //       firstName: formData.firstName,
-    //       lastName: formData.lastName,
-    //       email: formData.email,
-    //       phoneNumber: formData.phone,
-    //       location: formData.address,
-    //       summary: formData.summary,
-    //       coverLetter: formData.coverLetter,
-    //       date: new Date().toISOString(),
-    //       educationList: JSON.parse(formData.educationList),
-    //       experienceList: JSON.parse(formData.experienceList),
-    //       image: `data:image/jpeg;base64,${imageBase64}`, // Store the image as a data URL
-    //       resume: `data:application/pdf;base64,${resumeBase64}`, // Store the resume as a data URL
-    //     };
-    //     console.log(req.body);
-    
-    //     // Insert the documents into the collection
-    //     await applicationsPostCollection.insertOne(documentsToInsert);
-    
-    //     return res.status(201).json({ message: "Application submitted successfully" });
-    //   } catch (error) {
-    //     console.error("Error submitting application:", error);
-    //     return res.status(500).json({
-    //       message: "An error occurred while submitting the application",
-    //     });
-    //   }
-    // });
+   
     app.post("/upload-new", async (req, res) => {
       try {
-        // Extract data from the request body
+     
         const {
           jobTitle,
           stage,
@@ -187,8 +100,8 @@ async function run() {
           coverLetter,
           educationList,
           experienceList,
-          imageData, // Base64-encoded image data
-          resumeData, // Base64-encoded resume data
+          imageData, 
+          resumeData,
         } = req.body;
     
         // Create a document to insert into your MongoDB collection
@@ -200,15 +113,15 @@ async function run() {
           firstName,
           lastName,
           email,
-          phoneNumber: phone, // Rename phone to phoneNumber
-          location: address, // Rename address to location
+          phoneNumber: phone, 
+          location: address, 
           summary,
           coverLetter,
           date: new Date().toISOString(),
           educationList: JSON.parse(educationList),
           experienceList: JSON.parse(experienceList),
-          image: imageData, // Store base64-encoded image data as a string
-          resume: resumeData, // Store base64-encoded resume data as a string
+          image: imageData, 
+          resume: resumeData, 
         };await applicationsPostCollection.insertOne(documentToInsert);
 
         return res
@@ -221,63 +134,6 @@ async function run() {
         });
       }
     });
-
-    //  for multer image and resume
-
-    // app.post(
-    //   "/upload",
-    //   upload.fields([{ name: "resume" }, { name: "image" }]),
-    //   async (req, res) => {
-    //     const formData = req.body;
-    //     const resumeFilePath = req.files.resume[0].filename; // Accessing resume filename
-    //     const imageFilePath = req.files.image[0].filename; // Accessing image filename
-    //     const educationList = JSON.parse(formData.educationList); // Parse educationList JSON
-    //     const experienceList = JSON.parse(formData.experienceList); // Parse experienceList JSON
-    //     const currentDate = new Date().toISOString();
-    //     try {
-    //       // Assuming you have a MongoDB connection named "db" and a collection named "applicationsPostCollection"
-    //       await applicationsPostCollection.insertOne({
-    //         jobTitle: formData.jobTitle,
-    //         stage: formData.stage,
-    //         jobPosterEmail: formData.jobPosterEmail,
-    //         jobId: formData.jobId,
-    //         firstName: formData.firstName,
-    //         lastName: formData.lastName,
-    //         email: formData.email,
-    //         phoneNumber: formData.phone,
-    //         location: formData.address,
-    //         summary: formData.summary,
-    //         resume: resumeFilePath,
-    //         coverLetter: formData.coverLetter,
-    //         image: imageFilePath,
-    //         date: currentDate,
-    //         educationList: educationList, // Save parsed educationList
-    //         experienceList: experienceList,
-    //         // stage: formData.stage,
-    //         // appliedJobId: formData.appliedJobId,
-    //       });
-
-    //       res
-    //         .status(201)
-    //         .json({ message: "Application submitted successfully" });
-    //     } catch (error) {
-    //       console.error("Error submitting application:", error);
-    //       res.status(500).json({
-    //         message: "An error occurred while submitting the application",
-    //       });
-    //     }
-    //   }
-    // );
-
-    // post data of a new user
-    // app.post("/user", async (req, res) => {
-    //   const query = req.body;
-    //   const result = await UserCollection.insertOne(query);
-    //   res.send(result);
-    // });
-
-    // sobuj code
-    // save applicants
 
     app.put("/users/:email", async (req, res) => {
       const email = req.params.email;
@@ -462,15 +318,6 @@ async function run() {
       res.send(result);
     });
 
-    // get all application
-
-    // app.get("/all-applications/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) };
-    //   const result = await applicationsPostCollection.findOne(query);
-    //   res.send(result);
-
-    // });
     app.get("/all-applications/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -605,7 +452,6 @@ async function run() {
       res.send(result);
     });
 
-    // resume app.use('/uploads', upload.array("image", "resume"));
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
